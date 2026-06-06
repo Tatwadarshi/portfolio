4b1bbb8bdb55721df8ad70f3dadd38e17ae5952d
tatwadarshi <electromagneticwave16@gmail.com>
2026-06-06 18:21:34 +0530

    Added blurred glassy style to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,46 +19,46 @@ const NavBar = () => {
 
   return (
     <header className={`navbar ${isScrolled ? 'scrolled' : 'not-scrolled'}`}>
-        <div className='inner'>
-            <div className='logo flex md:gap-4 gap-2 align-middle justify-center items-center'>
-              <a href={asset_server_base_url+"/th.jpg"} target='_blank'>
-                <img className='md:size-15 size-8 rounded-full' src="/th.jpg" alt="TH" />
-              </a>
-              <a href="#hero">
-                Tatwadarshi Hota
-              </a>
-            </div>
-
-            <nav className='desktop'>
-                <ul>
-                    {navLinks.map(({href, name}) => (
-                        <li key={name} className='group'>
-                            <a href={href}>
-                                <span className='text-xl'>{name}</span>
-                                <div className="underline" />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-
-            <Button
-            className='contact-btn group rounded-sm' 
-            id={"contactbtn"}
-            action={()=>{
-                  const target = document.getElementById("contact");
-                  if (target) {
-                    const offset = window.innerHeight * 0.15;
-                    const top = target.getBoundingClientRect().top + window.scrollY - offset;
-                    window.scrollTo({
-                      top,
-                      behavior: 'smooth'
-                    });
-                  }
-                }}
-            text={`Contact me`}
-            />
+      <div className='inner'>
+        <div className='logo flex md:gap-4 gap-2 align-middle justify-center items-center'>
+          <a href={asset_server_base_url + "/th.jpg"} target='_blank'>
+            <img className='md:size-15 size-8 rounded-full' src="/th.jpg" alt="TH" />
+          </a>
+          <a href="#hero">
+            Tatwadarshi Hota
+          </a>
         </div>
+
+        <nav className='desktop'>
+          <ul>
+            {navLinks.map(({ href, name }) => (
+              <li key={name} className='group'>
+                <a href={href}>
+                  <span className='text-xl'>{name}</span>
+                  <div className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <Button
+          className='contact-btn group rounded-sm'
+          id={"contactbtn"}
+          action={() => {
+            const target = document.getElementById("contact");
+            if (target) {
+              const offset = window.innerHeight * 0.15;
+              const top = target.getBoundingClientRect().top + window.scrollY - offset;
+              window.scrollTo({
+                top,
+                behavior: 'smooth'
+              });
+            }
+          }}
+          text={`Contact me`}
+        />
+      </div>
     </header>
   )
 }
